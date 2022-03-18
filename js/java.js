7d4accda134b1cfef718aeec5a2707bf51b8ee37
brunons28 <73445411+brunons28@users.noreply.github.com>
2022-03-18 15:15:35 -0300

    Add files via upload

---
+++ js/java.js
@@ -0,0 +1,13 @@
+function hidden_link()
+{
+    document.getElementById("menu1").style.visibility="hidden";
+    document.getElementById("menu2").style.visibility="visible";
+    document.getElementById("nav3").style.visibility="hidden";
+
+}
+function show_link()
+{
+    document.getElementById("menu1").style.visibility="visible";
+    document.getElementById("menu2").style.visibility="hidden";
+    document.getElementById("nav3").style.visibility="visible";
+}
